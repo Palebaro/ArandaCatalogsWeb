2d6a39c180875351aba9a2948b609e4b2bc4c9b8
Palebaro <barpadroid@gmail.com>
2022-06-28 13:33:59 -0500

    Ajustes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('IngeneoWeb app is running!');
+    expect(page.getTitleText()).toEqual('Catalogo Web app is running!');
   });
 
   afterEach(async () => {
